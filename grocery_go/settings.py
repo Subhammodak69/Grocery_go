6e31b67ec432ea4df84de781aa3e64b3ee0e3c45
subham modak <modaksubham866@gmail.com>
2026-03-15 19:19:25 +0530

    update

--- grocery_go/settings.py
+++ grocery_go/settings.py
@@ -14,7 +14,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env.bool('DEBUG', default=True)
+DEBUG = env.bool('DEBUG', default=False)
+
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
