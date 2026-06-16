1ac792122475019bf3e4c6ff82dfa5dc294ba222
subhammodak <modaksubham866@gmail.com>
2026-06-16 21:17:27 +0530

    rthjkj

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,16 @@
+#!/bin/bash
+
+# Create virtual environment
+python3 -m venv venv
+
+# Activate it
+source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
-python manage.py migrate
